90392114e1f025610c48cce4cc41bcb5cf3f19bf
Cory Latschkowski <koree@redhat.com>
2023-01-25 14:17:27 -0600

    add: aws scripts

---
+++ scripts/delete-aws-console-user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# USAGE: ./delete-console-user.sh $GROUPNAME $USERNAME
+
+aws iam delete-login-profile --user-name $2
+aws iam detach-user-policy --user-name $2 --policy-arn 'arn:aws:iam::aws:policy/AdministratorAccess'
+aws iam remove-user-from-group --user-name $2 --group-name $1
+aws iam delete-user --user-name $2
+aws iam detach-group-policy --group-name $1 --policy-arn 'arn:aws:iam::aws:policy/AdministratorAccess'
+aws iam delete-group --group-name $1
+ß
