09157f085955c683e75e48d5ceacfca790e72621
Cory Latschkowski <koree@redhat.com>
2023-10-21 18:21:49 -0500

    update: all the things

--- docs/HELM.md
+++ docs/HELM.md
@@ -6,10 +6,10 @@ Download helm chart locally and dump yaml
 mkdir -p scratch/yaml
 cd scratch
 
-# helm <v3
-helm fetch --untar --untardir . 'rh-ecosystem-edge/console-plugin-nvidia-gpu' 
-helm template --output-dir './yaml' './console-plugin-nvidia-gpu'
-
 helm template rh-ecosystem-edge/console-plugin-nvidia-gpu
 helm template --output-dir './yaml' rh-ecosystem-edge/console-plugin-nvidia-gpu
+
+# helm < v3
+helm fetch --untar --untardir . 'rh-ecosystem-edge/console-plugin-nvidia-gpu' 
+helm template --output-dir './yaml' './console-plugin-nvidia-gpu'
 ```
--- scripts/library/nvidia.sh
+++ scripts/library/nvidia.sh
@@ -2,23 +2,27 @@
 
 nvidia_setup_dashboard_monitor(){
   curl -sLfO https://github.com/NVIDIA/dcgm-exporter/raw/main/grafana/dcgm-exporter-dashboard.json
-  oc create configmap nvidia-dcgm-exporter-dashboard -n openshift-config-managed --from-file=dcgm-exporter-dashboard.json || true
-  oc label configmap nvidia-dcgm-exporter-dashboard -n openshift-config-managed "console.openshift.io/dashboard=true" --overwrite
-  oc label configmap nvidia-dcgm-exporter-dashboard -n openshift-config-managed "console.openshift.io/odc-dashboard=true" --overwrite
+  oc -n openshift-config-managed create configmap nvidia-dcgm-exporter-dashboard --from-file=dcgm-exporter-dashboard.json || true
+  oc -n openshift-config-managed label configmap nvidia-dcgm-exporter-dashboard "console.openshift.io/dashboard=true" --overwrite
+  oc -n openshift-config-managed label configmap nvidia-dcgm-exporter-dashboard "console.openshift.io/odc-dashboard=true" --overwrite
   oc -n openshift-config-managed get cm nvidia-dcgm-exporter-dashboard --show-labels
   rm dcgm-exporter-dashboard.json
 }
 
 nvidia_setup_dashboard_admin(){
   helm repo add rh-ecosystem-edge https://rh-ecosystem-edge.github.io/console-plugin-nvidia-gpu || true
-  helm repo update
-  helm upgrade --install -n nvidia-gpu-operator console-plugin-nvidia-gpu rh-ecosystem-edge/console-plugin-nvidia-gpu
+  helm repo update > /dev/null 2>&1
+  helm upgrade --install -n nvidia-gpu-operator console-plugin-nvidia-gpu rh-ecosystem-edge/console-plugin-nvidia-gpu > /dev/null 2>&1
 
-  oc get consoles.operator.openshift.io cluster --output=jsonpath="{.spec.plugins}" || true
-  oc patch consoles.operator.openshift.io cluster --patch '{ "spec": { "plugins": ["console-plugin-nvidia-gpu"] } }' --type=merge || true
-  oc patch consoles.operator.openshift.io cluster --patch '[{"op": "add", "path": "/spec/plugins/-", "value": "console-plugin-nvidia-gpu" }]' --type=json || true
-  oc patch clusterpolicies.nvidia.com gpu-cluster-policy --patch '{ "spec": { "dcgmExporter": { "config": { "name": "console-plugin-nvidia-gpu" } } } }' --type=merge || true
-  oc -n nvidia-gpu-operator get all -l app.kubernetes.io/name=console-plugin-nvidia-gpu
+  if oc get consoles.operator.openshift.io cluster --output=jsonpath="{.spec.plugins}" >/dev/null; then
+    oc patch consoles.operator.openshift.io cluster --patch '{ "spec": { "plugins": ["console-plugin-nvidia-gpu"] } }' --type=merge
+  else
+    oc get consoles.operator.openshift.io cluster --output=jsonpath="{.spec.plugins}" | grep -q console-plugin-nvidia-gpu || \
+      oc patch consoles.operator.openshift.io cluster --patch '[{"op": "add", "path": "/spec/plugins/-", "value": "console-plugin-nvidia-gpu" }]' --type=json
+  fi
+
+  oc patch clusterpolicies.nvidia.com gpu-cluster-policy --patch '{ "spec": { "dcgmExporter": { "config": { "name": "console-plugin-nvidia-gpu" } } } }' --type=merge
+  oc -n nvidia-gpu-operator get deploy -l app.kubernetes.io/name=console-plugin-nvidia-gpu
 }
 
 nvidia_setup_mig_config(){
