a8384ae07044ea67f5ba8d8ea3360e527cac86aa
Cory Latschkowski <koree@redhat.com>
2024-04-16 21:17:45 -0500

    rename: cluster

--- scripts/wip/workshop_functions.sh
+++ scripts/wip/workshop_functions.sh
@@ -32,6 +32,7 @@ htpasswd_add_user(){
 
 htpasswd_get_file(){
   HTPASSWD=${1:-scratch/htpasswd-local}
+
   oc -n openshift-config \
     extract secret/"${HTPASSWD##*/}" \
     --keys=htpasswd \
@@ -40,127 +41,114 @@ htpasswd_get_file(){
 
 htpasswd_set_file(){
   HTPASSWD=${1:-scratch/htpasswd-local}
+
   oc -n openshift-config \
     set data secret/"${HTPASSWD##*/}" \
     --from-file=htpasswd="${HTPASSWD}"
 }
 
-workshop_init(){
-  TOTAL=${1:-25}
-  LIST=$(eval echo "{0..${TOTAL}}" )
+workshop_usage(){
+  echo ""
+  echo "Usage: workshop_[setup,clean,reset] [number of users]
+  "
+}
 
+workshop_init(){
   # do you have oc
-  which oc > /dev/null || return 1
+  which oc || return 1
 
   # do you have htpasswd
-  if ! which htpasswd >/dev/null; then
-    echo "error: install htpasswd"
-    return 1
-  fi
+  which htpasswd || return 1
 
   # create generated folder
   [ -n  "${OBJ_DIR}" ] && rm -rf "${OBJ_DIR}"
   [ ! -d "${OBJ_DIR}" ] && mkdir -p "${OBJ_DIR}"
 
-  oc apply -k workshop/overlays/default
-  htpasswd_set_file
+  echo "Workshop: Functions Loaded"
+  workshop_usage
+}
+
+workshop_create_group(){
+  OCP_GROUP=${1:-workshop-users}
+
+cat << YAML | oc apply -f -
+kind: Group
+apiVersion: user.openshift.io/v1
+metadata:
+  name: ${OCP_GROUP}
+users: []
+YAML
 }
 
 workshop_add_user_to_group(){
-  USER=${1:-admin}
-  OCP_ADMIN_GROUP=${2:-workshop-users}
-  
+  USER=${1:-user0}
+  OCP_GROUP=${2:-workshop-users}
+
+  if ! oc get group "${OCP_GROUP}" > /dev/null; then
+    workshop_create_group "${OCP_GROUP}"
+  fi
+
   oc adm groups add-users \
-  "${OCP_ADMIN_GROUP}" "${USER}"
+    "${OCP_GROUP}" "${USER}"
 }
 
-workshop_create_user_htpasswd(){
-  echo "
-    HTPASSWD_FILE: ${HTPASSWD_FILE}
-  "
+workshop_create_users(){
+  TOTAL=${1:-25}
+  LIST=$(eval echo "{0..${TOTAL}}")
 
   for i in ${LIST[@]}
   do
+
+    # create login things
     htpasswd_add_user "${DEFAULT_USER}${i}" "${DEFAULT_PASS}${i}" "${HTPASSWD_FILE}"
-  done
+    workshop_add_user_to_group "${DEFAULT_USER}${i}" "${DEFAULT_GROUP}"
 
-  htpasswd_set_file "${HTPASSWD_FILE}"
-}
-
-workshop_create_user_ns(){
-  OBJ_DIR=${TMP_DIR}/workshop
-  [ ! -d "${OBJ_DIR}" ] && mkdir -p "${OBJ_DIR}"
-
-  for i in ${LIST[@]}
-  do
+    # create users objs from template
     cp -a workshop/instance "${OBJ_DIR}/${DEFAULT_USER}${i}"
     sed -i 's/user0/'"${DEFAULT_USER}${i}"'/g' "${OBJ_DIR}/${DEFAULT_USER}${i}/"*.yaml
     # oc apply -f "${OBJ_DIR}/${DEFAULT_USER}${i}/user-ns.yaml"
     oc apply -k "${OBJ_DIR}/${DEFAULT_USER}${i}"
-    workshop_add_user_to_group "${DEFAULT_USER}${i}" "${DEFAULT_GROUP}"
 
-
-    # create ns
-    # oc -o yaml --dry-run=client \
-    #   create ns "${DEFAULT_USER}${i}" > "${OBJ_DIR}/${DEFAULT_USER}${i}-ns.yaml"
-    # oc apply -f "${OBJ_DIR}/${DEFAULT_USER}${i}-ns.yaml"
-    # workshop_add_user_to_group "${DEFAULT_USER}${i}" "${DEFAULT_GROUP}"
-
-    # create role binding - admin for user
-    # oc -o yaml --dry-run=client \
-    #   -n "${DEFAULT_USER}${i}" \
-    #   create rolebinding "${DEFAULT_USER}${i}-admin" \
-    #   --user "${DEFAULT_USER}${i}" \
-    #   --clusterrole admin > "${OBJ_DIR}/${DEFAULT_USER}${i}-ns-rb-admin.yaml"
-
-    # create role binding - view for workshop group
-    # oc -o yaml --dry-run=client \
-    #   -n "${DEFAULT_USER}${i}" \
-    #   create rolebinding "${DEFAULT_USER}${i}-view" \
-    #   --group "${DEFAULT_GROUP}" \
-    #   --clusterrole view > "${OBJ_DIR}/${DEFAULT_USER}${i}-ns-rb-view.yaml"
   done
 
-  # apply objects created in scratch dir
-    # oc apply \
-    #   -f "${OBJ_DIR}"
+  # update htpasswd in cluster
+  htpasswd_set_file "${HTPASSWD_FILE}"
+
 }
 
-workshop_clean_user_ns(){
-  for i in ${LIST[@]}
-  do
-    oc delete project "${DEFAULT_USER}${i}"
-  done
-}
-
-workshop_clean_user_notebooks(){
+workshop_clean_active_notebooks(){
   oc -n rhods-notebooks \
     delete po -l app=jupyterhub
+}
+
+workshop_clean_users(){
+  oc delete project -l owner=workshop
+  oc delete group "${DEFAULT_GROUP}"
+  oc delete identities,users --all
 }
 
 workshop_setup(){
   TOTAL=${1:-25}
 
   echo "Workshop: Setup"
-  workshop_init "${TOTAL}"
-  workshop_create_user_htpasswd
-  workshop_create_user_ns
+
+  oc apply -k workshop/overlays/default
+
+  htpasswd_set_file
+  workshop_create_users
 }
 
 workshop_clean(){
   echo "Workshop: Clean"
-  workshop_clean_user_ns
-  workshop_clean_user_notebooks
-  oc delete group "${DEFAULT_GROUP}"
-  oc delete identities,users --all
+  workshop_clean_active_notebooks
+  workshop_clean_users
 }
 
 workshop_reset(){
   echo "Workshop: Reset"
   workshop_clean
+  sleep 10
   workshop_setup
 }
 
-echo "Workshop: Functions Loaded"
-echo ""
-echo "usage: workshop_[setup,reset,clean] [number of users]"
+workshop_init
