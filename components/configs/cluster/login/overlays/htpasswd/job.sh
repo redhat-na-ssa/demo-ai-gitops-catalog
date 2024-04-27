936ad604e271dbda810898ace9db9723e6f82839
Cory Latschkowski <koree@redhat.com>
2024-04-27 09:58:04 -0500

    add: all

---
+++ components/configs/cluster/login/overlays/htpasswd/job.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# set -x
+
+init-htpasswd(){
+  OBJECT=${OBJECT:-secret/htpasswd-local}
+
+  # check for secret
+  if oc -n "${NS}" get "${OBJECT}" >/dev/null 2>&1; then
+    echo "success: ${OBJECT} in ${NS}"
+  else
+    echo "create: ${OBJECT} in namespace ${NS}"
+    oc apply -f /scripts/htpasswd-secret.yaml
+  fi
+}
+
+init-htpasswd
