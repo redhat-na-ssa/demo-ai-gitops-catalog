7ff2649af0915f6a37c2c4494a88270bd919c8dc
Cory Latschkowski <koree@redhat.com>
2024-06-03 05:11:36 -0500

    add: demo kludge

---
+++ demos/wip/kludge-parasol/setup_users.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MINIO_ROOT_USER=$(oc get secret minio-root-user -n ic-shared-minio -o template --template '{{.data.MINIO_ROOT_USER|base64decode}}')
+MINIO_ROOT_PASSWORD=$(oc get secret minio-root-user -n ic-shared-minio -o template --template '{{.data.MINIO_ROOT_PASSWORD|base64decode}}')
+MINIO_HOST=https://$(oc get route minio-s3 -n ic-shared-minio -o template --template '{{.spec.host}}')
+DASHBOARD_ROUTE=https://$(oc get route rhods-dashboard -n redhat-ods-applications -o jsonpath='{.spec.host}')
+
+oc apply -k instance
