66c8356dd807651ddd75025cfbf85bc37d080fd8
Cory Latschkowski <koree@redhat.com>
2024-06-04 17:07:46 -0500

    demo: parasol (#88)

--- components/operators/gpu-operator-certified/instance/components/aws-gpu-machineset/job.sh
+++ components/operators/gpu-operator-certified/instance/components/aws-gpu-machineset/job.sh
@@ -3,6 +3,8 @@
 # shellcheck disable=SC1091
 . /scripts/ocp.sh
 
+INSTANCE_TYPE=${INSTANCE_TYPE:-g4dn.4xlarge}
+
 ocp_aws_cluster || exit 0
-ocp_aws_create_gpu_machineset
+ocp_aws_create_gpu_machineset "${INSTANCE_TYPE}"
 ocp_create_machineset_autoscale
