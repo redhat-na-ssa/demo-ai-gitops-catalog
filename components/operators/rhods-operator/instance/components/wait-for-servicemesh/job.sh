476d6bb1f374f908a33fd8d8adf1f8e0b3b6d6f6
Cory Latschkowski <koree@redhat.com>
2024-10-30 13:05:17 -0500

    update: patch wait for

--- components/operators/rhods-operator/instance/components/wait-for-servicemesh/job.sh
+++ components/operators/rhods-operator/instance/components/wait-for-servicemesh/job.sh
@@ -3,12 +3,20 @@ set -e
 
 TIMEOUT_SECONDS=60
 
+patch_approval(){
+  APPROVAL=${1:-Automatic}
+
+  oc -n redhat-ods-operator \
+  patch subscription rhods-operator \
+  --type=merge --patch '{"spec":{"installPlanApproval":"'"${APPROVAL}"'"}}'
+}
+
 wait_for_service_mesh(){
   echo "Checking status of all service_mesh pre-reqs"
 
 SERVICEMESH_RESOURCES=(
-    crd/knativeservings.operator.knative.dev:condition=established \
-    crd/servicemeshcontrolplanes.maistra.io:condition=established \
+    crd/knativeservings.operator.knative.dev:condition=established
+    crd/servicemeshcontrolplanes.maistra.io:condition=established
   )
 
   for crd in "${SERVICEMESH_RESOURCES[@]}"
@@ -17,16 +25,13 @@ SERVICEMESH_RESOURCES=(
     CONDITION=$(echo "$crd" | cut -d ":" -f 2)
 
     echo "Waiting for ${RESOURCE} state to be ${CONDITION}..."
-    oc wait --for="${CONDITION}" "${RESOURCE}" --timeout="${TIMEOUT_SECONDS}s"
 
+    oc wait --for="${CONDITION}" "${RESOURCE}" --timeout="1s" 2>/dev/null && continue
+    patch_approval Manual
+
+    oc wait --for="${CONDITION}" "${RESOURCE}" --timeout="${TIMEOUT_SECONDS}s"
+    patch_approval Automatic
   done
 }
 
-approve_install(){
-    oc -n redhat-ods-operator \
-    patch subscription rhods-operator \
-    --type=merge --patch '{"spec":{"installPlanApproval":{"Automatic"}}}'
-}
-
 wait_for_service_mesh
-approve_install
