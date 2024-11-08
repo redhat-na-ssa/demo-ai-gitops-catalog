0086be1e49429883f46519a6f3d9278c0592b31b
Cory Latschkowski <koree@redhat.com>
2024-11-07 18:49:58 -0600

    update: rhoai

---
+++ demos/components/rhoai/approve-after-servicemesh/job.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+set -e
+
+TIMEOUT_SECONDS=60
+
+approve_installplan(){
+  echo -n 'Waiting for RHOAI install plan...'
+  until oc -n redhat-ods-operator get installplan -l operators.coreos.com/rhods-operator.redhat-ods-operator -o name >/dev/null 2>&1
+  do
+    echo -n .
+    sleep 5
+  done; echo
+
+  INSTALL_PLAN=$(oc -n redhat-ods-operator get installplan -l operators.coreos.com/rhods-operator.redhat-ods-operator -o name)
+  oc -n redhat-ods-operator \
+    patch "${INSTALL_PLAN}" \
+    --type=merge --patch '{"spec":{"approved":true}}'
+}
+
+patch_approval(){
+  APPROVAL=${1:-Automatic}
+
+  echo -n 'Waiting for RHOAI subscription...'
+  until oc get -n redhat-ods-operator subscription rhods-operator -o name >/dev/null 2>&1
+  do
+    echo -n .
+    sleep 5
+  done; echo
+
+  oc -n redhat-ods-operator \
+    patch subscription rhods-operator \
+    --type=merge --patch '{"spec":{"installPlanApproval":"'"${APPROVAL}"'"}}'
+}
+
+wait_for_service_mesh(){
+  echo "Checking status of all service_mesh pre-reqs"
+
+  SERVICEMESH_RESOURCES=(
+    crd/knativeservings.operator.knative.dev:condition=established
+    crd/servicemeshcontrolplanes.maistra.io:condition=established
+    crd/servicemeshmembers.maistra.io:condition=established
+  )
+
+  for crd in "${SERVICEMESH_RESOURCES[@]}"
+  do
+    RESOURCE=$(echo "$crd" | cut -d ":" -f 1)
+    CONDITION=$(echo "$crd" | cut -d ":" -f 2)
+
+    echo "Waiting for ${RESOURCE} state to be ${CONDITION}..."
+    oc wait --for="${CONDITION}" "${RESOURCE}" --timeout="${TIMEOUT_SECONDS}s" >/dev/null 2>&1
+  done
+}
+
+wait_for_service_mesh
+patch_approval
+approve_installplan
