b0ab43465066d0bb36e7e1c62990051cade92ec2
Cory Latschkowski <koree@redhat.com>
2024-11-14 10:13:42 -0600

    update: script

--- components/cluster-configs/adhoc-admin/base/job.sh
+++ components/cluster-configs/adhoc-admin/base/job.sh
@@ -9,7 +9,11 @@ run_adhoc(){
     "${ADHOC_SCRIPT}"
   else
     echo "missing: ${ADHOC_SCRIPT}"
-    echo "You are using this wrong. But you are brave, because you are trying new things!"
+    echo "
+      You are using this wrong.
+
+      But you are brave, because you are trying new things!
+    "
     return 0
   fi
 }
