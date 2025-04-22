0ce7eadba31e820408d19c6d350f0e33b1f6bafc
Cory Latschkowski <koree@redhat.com>
2025-04-21 17:37:50 -0700

    update: scripts

--- scripts/library/common.sh
+++ scripts/library/common.sh
@@ -1,36 +1,11 @@
 #!/bin/bash
 
-__run_all_functions(){
-  [ -e "/tmp/test-fun" ] || get_functions | grep -E -v 'argo|^_' > /tmp/test-fun
-
-  # shellcheck disable=SC2013
-  for i in $(grep -v '^ *#' /tmp/test-fun)
-  do
-    echo -e "${RED}DEBUG: ${ORANGE}$i${NC}"
-    $i && sed -i "/$i/d" /tmp/test-fun
-  done
-}
-
-genpass(){
-  < /dev/urandom LC_ALL=C tr -dc Aa-zZ0-9 | head -c "${1:-32}"
-}
-
 apply_config(){
   if [ ! -f "$1" ]; then
     echo "${1:-file} not found"
     return 1
   fi
   retry oc apply -f "$1" 2>/dev/null
-}
-
-apply_kustomize(){
-  if [ ! -f "${1}/kustomization.yaml" ]; then
-    echo "Please provide a dir with 'kustomization.yaml'"
-    echo "'kustomization.yaml' not found in ${1}"
-    return 1
-  fi
-
-  retry oc apply -k "$1" 2>/dev/null
 }
 
 apply_firmly(){
@@ -44,39 +19,14 @@ apply_firmly(){
   until_true oc apply -k "${1}" 2>/dev/null
 }
 
-until_true(){
-  echo "Running:" "${@}"
-  echo "Press <ctrl> + c to cancel"
-  until "${@}" 1>&2
-  do
-    echo "again..."
-    sleep 20
-  done
+apply_kustomize(){
+  if [ ! -f "${1}/kustomization.yaml" ]; then
+    echo "Please provide a dir with 'kustomization.yaml'"
+    echo "'kustomization.yaml' not found in ${1}"
+    return 1
+  fi
 
-  echo "[OK]"
-}
-
-retry(){
-  local attempts=30
-  local delay=20
-
-  echo "Running:" "${@}"
-  echo "Attempts:  ${attempts}"
-  echo "Delay:     ${delay}s"
-
-  # until "${@}" 1>&2
-  until "${@}"
-  do
-    if [[ $attempts -gt "1" ]]; then
-      ((attempts--))
-      echo "Remaining attempts: $attempts - waiting ${delay}s"
-      sleep $delay
-    else
-      echo "[FAILED]"
-        return 1
-    fi
-  done
-  echo "[OK]"
+  retry oc apply -k "$1" 2>/dev/null
 }
 
 function_extract(){
@@ -107,4 +57,43 @@ function_sort_file(){
   done
 
   mv tmp "${FILE}"
+}
+
+genpass(){
+  < /dev/urandom LC_ALL=C tr -dc Aa-zZ0-9 | head -c "${1:-32}"
+}
+
+retry(){
+  local attempts=30
+  local delay=20
+
+  echo "Running:" "${@}"
+  echo "Attempts:  ${attempts}"
+  echo "Delay:     ${delay}s"
+
+  # until "${@}" 1>&2
+  until "${@}"
+  do
+    if [[ $attempts -gt "1" ]]; then
+      ((attempts--))
+      echo "Remaining attempts: $attempts - waiting ${delay}s"
+      sleep $delay
+    else
+      echo "[FAILED]"
+        return 1
+    fi
+  done
+  echo "[OK]"
+}
+
+until_true(){
+  echo "Running:" "${@}"
+  echo "Press <ctrl> + c to cancel"
+  until "${@}" 1>&2
+  do
+    echo "again..."
+    sleep 20
+  done
+
+  echo "[OK]"
 }
