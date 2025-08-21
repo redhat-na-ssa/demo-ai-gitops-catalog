1de9b34e91c25e41567ba034f824000d49b662dd
Cory Latschkowski <koree@redhat.com>
2025-08-21 13:19:36 -0600

    fix: k8s scripts

--- scripts/library/k8s.sh
+++ scripts/library/k8s.sh
@@ -3,6 +3,15 @@
 
 which oc >/dev/null && alias kubectl=oc
 
+# do core resources first, which are at a separate api location
+k8s_api_dump_core(){
+  SERVER=${1:-http://localhost:8080}
+  api="core"
+  curl -s "${SERVER}/api/v1" | \
+    jq -r --arg api "$api" \
+    '.resources | .[] | "\($api) \(.name): [ \(.verbs | join(",")) ]"'
+}
+
 k8s_api_dump_noncore(){
   SERVER=${1:-http://localhost:8001}
   APIS=$(curl -s "${SERVER}/apis" | jq -r '[.groups | .[].name] | join(" ")')
